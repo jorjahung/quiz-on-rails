20ed8b0d426ac083742562b70e9294f279ce6d41
Jorja Hung <jorjahung@gmail.com>
2014-02-26 23:36:47 +0000

    Fixed password error duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
-  has_secure_password 
-  validates :password, presence: { on: :create }, length: { minimum: 5, allow_blank: true }
-  validates :username,
+	validates :username,
             presence: true,
             uniqueness: true,
             length: { minimum: 3}
+
+  has_secure_password 
+
+	validates :password, 
+						presence: { on: :create }, 
+						length: { minimum: 5, allow_blank: true },
+						allow_blank: true
+
 
 
 end
