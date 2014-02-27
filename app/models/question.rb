9d292a0846542353e73b7b02bc479ca3b24e8dd2
Colin Frankish <colin.frankish@gmail.com>
2014-02-27 15:46:46 +0000

    User cannot see own question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ActiveRecord::Base
   validates_presence_of :body
   belongs_to :user
+
+  def self.randomize
+    self.order("RANDOM()").first
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should give us a random question' do    
+    question_one = Question.create!(body: "The Eiffel Tower is in Paris. (T/F)", answer: true)
+    question_two = Question.create!(body: "The Eiffel Tower is not in Paris. (T/F)", answer: false)
+    expect(Question).to receive(:order).with('RANDOM()').and_return([question_one, question_two])
+    random = Question.randomize
+    expect(random.body).to eq(question_one.body)
+  end
 end
